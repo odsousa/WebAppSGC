5d5dc7f39b1660b0d1c8789fdc13aeaef5a736dc
Odailson Sousa <odailson.sousa@outlook.com>
2020-09-02 03:17:15 -0300

    Add project files.

---
+++ src/SGGC.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SGGC.UI.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
